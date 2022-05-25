2fd9cfb08dbcf10a6d1e102e8b4aa90fa170ec0c
TKG2800 <takumi1997iwamatsu@yahoo.co.jp>
2022-05-25 16:01:50 +0900

    更新(Update)

--- Assets/Scipt/MapGeneration.cs
+++ Assets/Scipt/MapGeneration.cs
@@ -4,21 +4,22 @@ using System.Collections.Generic;
 
 /// 穴掘りアルゴリズム
 public class MapGeneration : MonoBehaviour  {
-  int r_width;
-  int r_height;
+  [SerializeField]int r_width = 7;
+  [SerializeField]int r_height = 7;
   int way = 0;
   int wall = 1;
   int start = 2;
   int goal = 3;
+  Room layer;
 
   void Start() {
-
+    layer = Room(r_width,r_height);
   }
 
   void Update() {
 
   }
 
-  void Dig() {}
+  void Dig(Room layer,) {}
   void getSart() {}
   }
--- Assets/Scipt/Room.cs
+++ Assets/Scipt/Room.cs
@@ -1,10 +1,34 @@
 public class Room {
   int Width;
   int Height;
-  int[][] value;
-  Room( int width, int height ) {}
-  bool IsOutOfRange() {}
-  int Get() {}
-  void Set() {}
-  void Fill() {}
+  int outOfRange = -1;
+  int[][] values;
+  public Room( int width, int height ) {
+    if (width > 0 && height > 0) {
+      Width = width;
+      Height = height
+      values = new int[Width][Height];
+    }
+  }
+   public bool IsOutOfRange( int x, int y ) {
+    if(x < 0 || x >= Width) {return true;}
+    if(y < 0 || y >= Height) {return true;}
+    return false;
+  }
+  public int Get( int x, int y ) {
+    if(IsOutOfRange(x,y)) {
+      return outOfRange;
+    }
+    return values[x][y];
+  }
+  public void Set( int x, int y,int v) {
+    values[x][y] = v;
+  }
+  public void Fill(int val) {
+    for (int i = 0 ; i < Width ; i++ ) {
+      for (int j = 0; j < Height; j++ ) {
+        Set(i,j val);
+      }
+    }
+  }
 }
